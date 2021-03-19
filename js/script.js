b48a03b745ff907d970974a2e7e83ceeb55b1147
Anastasia <anastasiabogdan@bk.ru>
2021-03-19 21:02:14 +0300

    first_homework_about_film

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
 'use sctrict';
-// alert('meow')
 
-const result = confirm('Are you here?');
-console.log(result);
+let numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+let lastFilm = prompt('Один из последних просмотренных фильмов?');
+let rateFilm = prompt('На сколько оцените его?');
+let lastFilm2 = prompt('Один из последних просмотренных фильмов?');
+let rateFilm2 = prompt('На сколько оцените его?');
+
+personalMovieDB.movies[lastFilm] = rateFilm;
+personalMovieDB.movies[lastFilm2] = rateFilm2;
+console.log(personalMovieDB);
